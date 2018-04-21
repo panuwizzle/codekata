b4fa8ce78b00036573bd51f2a45e2be27a1ead1f
Moo <sukorn@gmail.com>
2018-04-21 15:49:55 +0700

    Bot app from Maqe quiz

---
+++ spec/Kata/BotSpec.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace spec\Kata;
+
+use Kata\Bot;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class BotSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Bot::class);
+    }
+
+    function it_should_be_able_to_reset_the_position()
+    {
+        $this->reset();
+        $this->getCurrentPosition()->shouldReturn([0, 0]);
+    }
+
+    function it_should_show_the_current_position()
+    {
+        $this->getCurrentPosition()->shouldReturn([0, 0]);
+    }
+
+    // try step to the north (not turning yet)
+    function it_should_walk_farward_1_step_north()
+    {
+        $this->reset();
+        $this->walk(1);
+        $this->getCurrentPosition()->shouldReturn([0, 1]);
+    }
+
+    function it_should_walk_farward_2_step_north()
+    {
+        $this->reset();
+        $this->walk(2);
+        $this->getCurrentPosition()->shouldReturn([0, 2]);
+    }
+
+    // try turn
+    function it_should_turn_left_and_face_west()
+    {
+        $this->reset();
+        $this->turn('L');
+        $this->getFacing()->shouldReturn('West');
+    }
+    function it_should_turn_right_and_face_east()
+    {
+        $this->reset();
+        $this->turn('R');
+        $this->getFacing()->shouldReturn('East');
+    }
+    function it_should_turn_left_and_face_south()
+    {
+        $this->reset();
+        $this->turn('L');
+        $this->turn('L');
+        $this->getFacing()->shouldReturn('South');
+    }
+
+    function it_should_walk_farward_1_step_east()
+    {
+        $this->reset();
+        $this->walk(1);
+        $this->getCurrentPosition()->shouldReturn([0, 1]);
+    }
+
+    function it_should_walk_farward_2_step_east()
+    {
+        $this->reset();
+        $this->walk(2);
+        $this->getCurrentPosition()->shouldReturn([0, 2]);
+    }
+}
---
+++ src/Kata/Bot.php
@@ -0,0 +1,124 @@
+<?php
+namespace Kata;
+
+/*
+ * A robot that walk by parsing the instruction
+ * L, R - turn left or right
+ * W2, WN - walk by the number after W
+ *
+ * example. RW2LLW3 = turn right, walk 2 steps, turn left 2 times and walk 3 step
+ * The robot will start at 0,0 and facing north
+ */
+class Bot
+{
+    private $currentPosition = [0, 0];
+    private $facing = 0;
+    private $directions = ['North', 'East', 'West', 'South'];
+    private $commandStack = [];
+
+    const DIRECTION_NORTH = 0;
+    const DIRECTION_EAST = 1;
+    const DIRECTION_WEST = 2;
+    const DIRECTION_SOUTH = 3;
+
+    public function resetCommand()
+    {
+        $this->commandStack = [];
+    }
+
+    public function setCommand($command)
+    {
+    }
+
+    private function parseCommand($command)
+    {
+    }
+
+    public function getCurrentPosition()
+    {
+        return $this->currentPosition;
+    }
+
+    public function getFacing()
+    {
+        return $this->directions[$this->facing];
+    }
+
+    public function reset()
+    {
+        $this->currentPosition = [0, 0];
+    }
+
+    public function walk($step)
+    {
+        // north
+        if ($this->facing == self::DIRECTION_NORTH) {
+            $this->currentPosition = [
+                $this->currentPosition[0], 
+                $this->currentPosition[1] + $step 
+            ];
+        }
+
+        // east
+        if ($this->facing == self::DIRECTION_EAST) {
+            $this->currentPosition = [
+                $this->currentPosition[0] + $step, 
+                $this->currentPosition[1]
+            ];
+        }
+
+        // west
+        if ($this->facing == self::DIRECTION_WEST) {
+            $this->currentPosition = [
+                $this->currentPosition[0] - $step, 
+                $this->currentPosition[1]
+            ];
+        }
+
+        // south
+        if ($this->facing == self::DIRECTION_SOUTH) {
+            $this->currentPosition = [
+                $this->currentPosition[0] + $step, 
+                $this->currentPosition[1]
+            ];
+        }
+    }
+
+    public function turn($direction)
+    {
+        if ($direction == 'L') {
+            switch ($this->facing) {
+            case self::DIRECTION_NORTH:
+                $this->facing = self::DIRECTION_WEST;
+                break;
+            case self::DIRECTION_EAST:
+                $this->facing = self::DIRECTION_NORTH;
+                break;
+            case self::DIRECTION_WEST:
+                $this->facing = self::DIRECTION_SOUTH;
+                break;
+            case self::DIRECTION_SOUTH:
+                $this->facing = self::DIRECTION_EAST;
+                break;
+            }
+        } 
+
+        if ($direction == 'R') {
+            switch ($this->facing) {
+            case self::DIRECTION_NORTH:
+                $this->facing = self::DIRECTION_EAST;
+                break;
+            case self::DIRECTION_EAST:
+                $this->facing = self::DIRECTION_SOUTH;
+                break;
+            case self::DIRECTION_WEST:
+                $this->facing = self::DIRECTION_NORTH;
+                break;
+            case self::DIRECTION_SOUTH:
+                $this->facing = self::DIRECTION_WEST;
+                break;
+            }
+        }
+    }
+}
+
