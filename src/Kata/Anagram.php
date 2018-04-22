262a5f09fade7e1749827475810d3cd7f50dbf22
Moo <sukorn@gmail.com>
2018-04-22 21:16:06 +0700

    Open with vscode and phpcs shout out about comment

--- src/Kata/Anagram.php
+++ src/Kata/Anagram.php
@@ -1,7 +1,36 @@
 <?php
+/**
+ * This file is part of my Kata
+ * file doc to shut phpcs up
+ * 
+ * @category Kata
+ * @package  Kata
+ * @author   Phanuwat <sukorn@gmail.com>
+ * @license  http://none.com none
+ * @link     http://none.com
+ */
+
 namespace Kata;
 
-class Anagram {
+/**
+ * Anagram class for check if the string is anagram
+ * 
+ * @category Kata
+ * @package  Kata
+ * @author   Phanuwat <sukorn@gmail.com>
+ * @license  http://none.com none
+ * @link     http://none.com
+ */
+class Anagram
+{
+    /**
+     * Function isAnagram function
+     * 
+     * @param string $str_a First string for compare
+     * @param string $str_b Second string for compare
+     * 
+     * @return bool
+     */
     function isAnagram($str_a, $str_b) 
     {
         //return (count_chars($str_a, 1) == count_chars($str_b, 1));
@@ -11,6 +40,14 @@ class Anagram {
         sort($ar2);
         return ($ar1 === $ar2) ? true : false;
     }
+
+    /**
+     * The sort function
+     * 
+     * @param string $str String to sort
+     * 
+     * @return string
+     */
     public function sort($str)
     {
         $ar = str_split($str);
@@ -18,6 +55,14 @@ class Anagram {
         return trim(implode('', $ar));
     }
 
+    /**
+     * The anagram function with custom sort
+     * 
+     * @param string $str1 First string to check
+     * @param string $str2 Second string to check
+     * 
+     * @return bool
+     */
     public function isAnagramWithSort($str1, $str2)
     {
         $s1 = $this->sort($str1);
