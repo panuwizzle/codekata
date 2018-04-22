53a9323b790c2ee939bde6016eceabceb8851780
Moo <sukorn@gmail.com>
2018-04-22 17:09:59 +0700

    Fix the test

--- tests/Kata/AnagramTest.php
+++ tests/Kata/AnagramTest.php
@@ -9,20 +9,16 @@ class AnagramTest extends TestCase
     public function testIsAnagram()
     {
         $obj = new Anagram();
-
-        $this->assertTrue($obj->isAnagram('belt', 'telb'));
-        $this->assertTrue($obj->isAnagram('america', 'america'));
-        $this->assertTrue($obj->isAnagram('Bernard Lowe', 'Arnold Weber'));
-
-        $this->assertFalse($obj->isAnagram('belt', 'yelb'));
-        $this->assertFalse($obj->isAnagram('america', 'xeamirac'));
-        $this->assertTrue($obj->isAnagram('abcde', 'cd eba'));
-        $this->assertTrue($obj->isAnagram('abcde', 'dceab'));
-        $this->assertTrue($obj->isAnagram('abcde', 'dcaeb'));
-        $this->assertTrue($obj->isAnagram('python', 'typhon'));
-
         $this->assertFalse($obj->isAnagram('abcde', 'dcaebd'));
         $this->assertFalse($obj->isAnagram('abcde', 'fffff'));
+        $this->assertFalse($obj->isAnagram('america', 'xeamirac'));
+        $this->assertFalse($obj->isAnagram('belt', 'yelb'));
+        $this->assertTrue($obj->isAnagram('abcde', 'cdeba'));
+        $this->assertTrue($obj->isAnagram('abcde', 'dcaeb'));
+        $this->assertTrue($obj->isAnagram('abcde', 'dceab'));
+        $this->assertTrue($obj->isAnagram('america', 'america'));
+        $this->assertTrue($obj->isAnagram('belt', 'telb'));
+        $this->assertTrue($obj->isAnagram('python', 'typhon'));
 
     }
 }
